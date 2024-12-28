558f6828dc1f0fb341ccfd6c9259e3a864bc8293
Dang-Minh Tran <dangtran@student.42.fr>
2024-12-28 23:33:34 +0100

    errors almost done

--- sources/errors.c
+++ sources/errors.c
@@ -6,14 +6,14 @@
 /*   By: dangtran <dangtran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 19:00:55 by dangtran          #+#    #+#             */
-/*   Updated: 2024/12/28 22:44:26 by dangtran         ###   ########.fr       */
+/*   Updated: 2024/12/28 23:28:38 by dangtran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../includes/so_long.h"
 
 // Check the horizontal walls
-static int  isHorizontalWall (t_complete *game) 
+static int isHorizontalWall(t_complete *game) 
 {
     int i;
     int j;
@@ -22,7 +22,7 @@ static int  isHorizontalWall (t_complete *game)
     j = 0;
 
     while (j < i) {
-        if (game->map[0][i] == 1 && game->map[game->height - 1][j] == 1)
+        if (game->map[0][i] == '1' && game->map[game->height - 1][j] == '1')
             return (0);
         j++;
     }
@@ -31,7 +31,7 @@ static int  isHorizontalWall (t_complete *game)
 }
 
 // Check the vertical walls
-static int  isVerticalWall (t_complete *game) 
+static int isVerticalWall(t_complete *game) 
 {
     int i;
     int j;
@@ -40,7 +40,7 @@ static int  isVerticalWall (t_complete *game)
     j = game->width;
 
     while (j < i) {
-        if (!(game->map[i][0] == 1 && game->map[i][j - 1] == 1))
+        if (!(game->map[i][0] == '1' && game->map[i][j - 1] == '1'))
             return (0);
         i++;
     }
@@ -48,8 +48,40 @@ static int  isVerticalWall (t_complete *game)
     return (1); 
 }
 
-// Check all the walls ?
+// Check all the walls
+static void areWalls(t_complete *game)
+{
+    int vertical;
+    int horizontal;
 
+    vertical = isVerticalWall(game);
+    horizontal = isHorizontalWall(game);
+
+    if (!vertical || !horizontal)
+    {
+        // TODO - RAJOUTER LA LIBFT OU JUSTE PRINTF ?
+        ft_printf("MISSING WALLS");
+        // TODO - COMMENT ON SORT D'ICI ?   
+    }
+}
+
+// Check if the map is correct
+static void check_map(t_complete *game, int height, int width)
+{
+    if (game->map[height][width] != '1' ||
+    game->map[height][width] != '0' ||
+        game->map[height][width] != 'C' ||
+            game->map[height][width] != 'E' ||
+                game->map[height][width] != 'P')
+                ft_printf("PROBLEME DANS L'ECRITURE DE LA MAP, ICI :", game->map[height][width]);
+                // TODO - COMMENT ON SORT D'ICI ?
+    if (game->map[height][width] == 'C')
+        game->columnNb++;
+    if (game->map[height][width] == 'E')
+        game->exitNb++;
+    if (game->map[height][width] == 'P')
+        game->playerNb++;
+}
 
 // verifier le chemin ?
 
